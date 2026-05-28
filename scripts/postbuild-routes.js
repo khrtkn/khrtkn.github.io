0cb62942a7d4662765c7e11ad42194406c161d9a
khr <kihiro.tokuno@gmail.com>
2026-05-29 05:15:00 +0900

    Add static route indexes for GitHub Pages

---
+++ scripts/postbuild-routes.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const buildDir = path.resolve(__dirname, '..', 'build');
+const indexFile = path.join(buildDir, 'index.html');
+
+const projectSlugs = [
+  'modoru-mirai-ya',
+  'TalkingMuseum',
+  'interactive-museum',
+  'lets-speak-with-hands',
+  'finca',
+];
+
+const routes = [
+  'works',
+  'about',
+  ...projectSlugs.map((slug) => `works/${slug}`),
+  'en',
+  'en/works',
+  'en/about',
+  ...projectSlugs.map((slug) => `en/works/${slug}`),
+];
+
+fs.copyFileSync(indexFile, path.join(buildDir, '404.html'));
+
+routes.forEach((route) => {
+  const routeDir = path.join(buildDir, route);
+  fs.mkdirSync(routeDir, { recursive: true });
+  fs.copyFileSync(indexFile, path.join(routeDir, 'index.html'));
+});
